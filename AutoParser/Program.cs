c278c22ea258c186a867adf8382e4d2ad436a0da
Александр Васильев <astrlex2004@gmail.com>
2022-06-03 12:54:01 +0400

    Добавьте файлы проекта.

---
+++ AutoParser/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoParser
+{
+    internal class Program
+    {
+        static void Format(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = list[i].Replace(@"\r", "");
+                list[i] = list[i].Replace("&#x2757;", "");
+                list[i] = list[i].Replace(@"\n", "");
+                list[i] = list[i].Replace("&#xfe0f;", "");
+                list[i] = list[i].Replace("&#x1f539;", "");
+                list[i] = list[i].Replace("&#x2705;", "");
+                list[i] = list[i].Replace("&#x2796;", "");
+                list[i] = list[i].Replace("&#x2033;", "");
+                list[i] = list[i].Replace("&#x1f699;", "");
+                list[i] = list[i].Replace("}", "");
+                list[i] = list[i].Replace("\"", "");
+                list[i] = list[i].Replace("name", "Название");
+                list[i] = list[i].Replace("description", "Описание");
+                list[i] = list[i].Replace("numberOfDoors", "Количество дверей");
+                list[i] = list[i].Replace("productionDate", "Год выпуска");
+                list[i] = list[i].Replace("vehicleTransmission", "Трансмиссия");
+                list[i] = list[i].Replace("datePublished", "Дата публикации");
+                list[i] = list[i].Replace("vehicleConfiguration", "Конфигурация ТС");
+                list[i] = list[i].Replace("bodyType", "Кузов");
+                list[i] = list[i].Replace("modelDate", "Дата выпуска модели");
+                list[i] = list[i].Replace("vehicleEngine", "Двигатель");
+                list[i] = list[i].Replace("EngineSpecification", "Спецификация:");
+                list[i] = list[i].Replace("engineDisplacement", "Объем двигателя");
+                list[i] = list[i].Replace("enginePower", "Мощность двигателя");
+                list[i] = list[i].Replace("fuelType", "Топливо");
+                list[i] = list[i].Replace("{@type:", "");
+                list[i] = list[i].Replace("brand", "Марка");
+                list[i] = list[i].Replace("color", "Цвет");
+                if(list[i].IndexOf("contentUrl") > 0)
+                    list[i] = list[i].Replace(list[i].Substring(list[i].IndexOf("contentUrl"), list[i].IndexOf(".jpg") - list[i].IndexOf("contentUrl")+5), "");
+                if(list[i].IndexOf("image") > 0)
+                    list[i] = list[i].Replace(list[i].Substring(list[i].IndexOf("image"), list[i].IndexOf("ct,") - list[i].IndexOf("contentUrl")+5), "");
+                
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            Console.Write("Mark name: ");
+            string Vehicle = Console.ReadLine();
+            Console.Write("Model name: ");
+            List<string> vehiclelist = new List<string>();
+            string CarModel = Console.ReadLine();
+            string URL = $"https://auto.drom.ru/{Vehicle}/{CarModel}";
+            HttpClient http = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await http.GetAsync(URL);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                int index = 0;
+                int pos = -1;
+                while ((pos = responseBody.IndexOf("\"name\"", index)) != -1) {
+                    if (responseBody.IndexOf("offers", pos) - pos - 2 > 0)
+                    {
+                        vehiclelist.Add(responseBody.Substring(pos, responseBody.IndexOf("offers", pos) - pos - 2));
+                    }
+                    else
+                        break;
+                    index = pos + 1;
+                }
+                Format(vehiclelist);
+                foreach(string vehicle in vehiclelist)
+                {
+                    Console.WriteLine(vehicle);
+                    Console.WriteLine("\n\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AutoParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("AutoParser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoParser")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("61402c31-baca-4a2c-b0db-fae8e82fa68a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
